9e5b6c5873e6abb291ae16b09726eb6cda470f37
Prathamwate <prathameshwate999@gmail.com>
2024-10-28 15:01:58 +0530

    Demo

---
+++ src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+  @GetMapping("/test")
+  String message() {
+    return "Hi this is from Test.";
+  }
+}
